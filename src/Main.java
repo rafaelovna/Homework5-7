fbd8933e788dc5d3355057bf82e03a12585f75f2
rafaelovna <aikanush777@mail.ru>
2022-12-08 19:07:57 +0400

    Задание выполнено.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task5();
+        task6();
+        task7();
+    }
+    public static void task5() {
+            System.out.println("Задача 5");
+        for (int i = 1904; i <= 2096; i = i + 4)
+            System.out.println(i+ " год является високосным");
+    }
+    public static void task6() {
+        System.out.println("Задача 6");
+        for (int f = 7; f <= 98; f = f + 7)
+            System.out.println(f);
+    }
+    public static void task7() {
+        System.out.println("Задача 7");
+        for (int a = 1; a <= 512; a = a * 2)
+            System.out.println(a);
     }
 }
